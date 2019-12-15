c0c4d80ceb019950dd3671ea8648505e89fd3e07
Nicolas PERANSIN <nicolas.peransin@obeo.fr>
2019-12-15 13:03:36 +0100

    Fix github.pages target and deploy env.

--- deployBinTray.sh
+++ deployBinTray.sh
@@ -2,20 +2,27 @@
 ### Necessary ENV variables ###
 # BINTRAY_USER=
 # BINTRAY_API_KEY=
-# BINTRAY_OWNER=
+# BINTRAY_SUBJECT=
 # BINTRAY_REPO=
 ### Usage
 # Sample Usage: pushToBintray.sh package version pathToP2Repo
 
-API=https://api.bintray.com
+
+BINTRAY_API=https://api.bintray.com/content/${BINTRAY_SUBJECT}/${BINTRAY_REPO}
+
+
 PCK_NAME=$1
 PCK_VERSION=$2
 PATH_TO_REPOSITORY=$3
 
 
 echo "==Infos"
-echo "BINTRAY_OWNER=${BINTRAY_OWNER}"
+echo "BINTRAY_SUBJECT=${BINTRAY_SUBJECT}"
 echo "BINTRAY_REPO=${BINTRAY_REPO}"
+# Sensible info
+echo [ -z "${BINTRAY_USER}" ] && echo "No user" || echo "User set"
+echo [ -z "${BINTRAY_API_KEY}" ] && echo "No api key" || echo "Api key set"
+
 echo "PCK_NAME=${PCK_NAME}"
 echo "PCK_VERSION=${PCK_VERSION}"
 echo "PATH_TO_REPOSITORY=${PATH_TO_REPOSITORY}"
@@ -39,7 +46,7 @@ for f in $FILES; do
   if [ ! -d $f ]; then
     echo "Processing $f file..."
       curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} \
-	      https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
+	      ${BINTRAY_API}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
     echo ""
   fi
 done
@@ -48,7 +55,7 @@ echo "Processing features dir $FEATUREDIR file..."
 for f in $FEATUREDIR; do
   echo "Processing feature: $f file..."
   curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} \
-      https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
+      ${BINTRAY_API}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
   echo ""
 done
 
@@ -58,7 +65,7 @@ for f in $PLUGINDIR; do
    # take action on each file. $f store current file name
   echo "Processing plugin: $f file..."
   curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} \
-      https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
+      ${BINTRAY_API}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
   echo ""
 done
 
@@ -68,12 +75,12 @@ if [ -d "$(dirname $BINARYDIR)" ]; then
      # take action on each file. $f store current file name
     echo "Processing binary: $f file..."
     curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} \
-	    https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
+	    ${BINTRAY_API}/${PCK_NAME}/${PCK_VERSION}/$f;publish=0;override=1
     echo ""
   done
 fi
 
 echo "Publishing the new version"
 curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} \
-    https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
+    ${BINTRAY_API}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
 
